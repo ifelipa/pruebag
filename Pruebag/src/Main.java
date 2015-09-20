0c589847cffa991f2e09533317308f8cc32f5038
Jordi Felip <jordifelipzayas@gmail.com>
2015-09-20 17:49:41 +0200

    add comment

--- Pruebag/src/Main.java
+++ Pruebag/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
  public static void main(String[] args) {
 	System.out.println("Hola Jordi");
 	System.out.println("Hola,isma");
+	System.out.println("Probando desde eclipse");
 }
 }
